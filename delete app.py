4227151960b387b494f35f328d14d67299dc76b9
Priyanshi240402 <82384025+Priyanshi240402@users.noreply.github.com>
2024-02-05 22:55:33 +0530

    Update delete app.py

--- delete app.py
+++ delete app.py
@@ -1,16 +1,1 @@
-import requests
-import json
 
-url = "https://api.promulgateinnovations.com/api/v1/fetchYoutubeAnalytics"
-
-payload = json.dumps({
-  "channelId": "UCq-Fj5jknLsUf-MWSy4_brA"
-})
-headers = {
-  'Authorization': 'Basic cHJvbXVsZ2F0ZTpwcm9tdWxnYXRl',
-  'Content-Type': 'application/json'
-}
-
-response = requests.post(url, headers=headers, data=payload)
-
-print(response.text)
