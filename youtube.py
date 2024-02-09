d18b9cdf2540ff28da4a66938d8e16fa5169e2e3
Priyanshi240402 <82384025+Priyanshi240402@users.noreply.github.com>
2024-02-09 19:52:01 +0530

    scrapping.py

--- youtube.py
+++ youtube.py
@@ -7,72 +7,84 @@ import json
 
 app = Flask(__name__)
 
-def send_data_to_external_api(channelId, videos_published, audience, views, whatch_time, total_likes, playlists, audience_retention, recent_videos, audience_by_countries, audience_by_demographics, traffic_source, external_source):
-    url = "https://api.promulgateinnovations.com/api/v1/setYoutubeAnalytics"
+analytics_url = "https://api.promulgateinnovations.com/api/v1/setYoutubeAnalytics"
+
+analytics_headers = {
+    'Authorization': 'Basic cHJvbXVsZ2F0ZTpwcm9tdWxnYXRl',
+    'Content-Type': 'application/json'
+}
+
+def scrape_channel_data(page_soup, channel_id):
+    
     payload = {
-        "channelId": channelId,
-        "views": views,
-        "whatch_time": whatch_time,
-        "videos_published": videos_published,
-        "total_likes": total_likes,
-        "playlists": playlists,
-        "audience_retention": audience_retention,
-        "recent_videos": recent_videos,
-        "audience_by_countries": audience_by_countries,
-        "audience_by_demographics": audience_by_demographics,
-        "traffic_source": traffic_source,
-        "external_source": external_source,
-        "audience": audience,  # Include the audience variable here
+        "channelId": channel_id,
+        "views": page_soup.find("span", {"id": "youtube-stats-header-views"}).text if page_soup.find("span", {"id": "youtube-stats-header-views"}) else None,
+        "whatch_time": page_soup.find("span", {"id": "youtube-stats-header-whatch_time"}).text if page_soup.find("span", {"id": "youtube-stats-header-whatch_time"}) else None,
+        "videos_published": page_soup.find("span", {"id": "youtube-stats-header-uploads"}).text if page_soup.find("span", {"id": "youtube-stats-header-uploads"}) else None,
+        "total_likes": page_soup.find("span", {"id": "youtube-stats-header-total_likes"}).text if page_soup.find("span", {"id": "youtube-stats-header-total_likes"}) else None,
+        "playlists": page_soup.find("span", {"id": "youtube-stats-header-playlists"}).text if page_soup.find("span", {"id": "youtube-stats-header-playlists"}) else None,
+        "audience_retention": page_soup.find("span", {"id": "youtube-stats-header-audience_retention"}).text if page_soup.find("span", {"id": "youtube-stats-header-audience_retention"}) else None,
+        "recent_videos": page_soup.find("span", {"id": "youtube-stats-header-recent_videos"}).text if page_soup.find("span", {"id": "youtube-stats-header-recent_videos"}) else None,
+        "audience_by_countries": page_soup.find("span", {"id": "youtube-stats-header-audience_by_countries"}).text if page_soup.find("span", {"id": "youtube-stats-header-audience_by_countries"}) else None,
+        "audience_by_demographics": page_soup.find("span", {"id": "youtube-stats-header-audience_by_demographics"}).text if page_soup.find("span", {"id": "youtube-stats-header-audience_by_demographics"}) else None,
+        "traffic_source": page_soup.find("span", {"id": "youtube-stats-header-traffic_source"}).text if page_soup.find("span", {"id": "youtube-stats-header-traffic_source"}) else None,
+        "external_source": page_soup.find("span", {"id": "youtube-stats-header-external_source"}).text if page_soup.find("span", {"id": "youtube-stats-header-external_source"}) else None,
+        "audience": page_soup.find("span", {"id": "youtube-stats-header-subs"}).text if page_soup.find("span", {"id": "youtube-stats-header-subs"}) else None,
         "addedBy": "Priyanshi"
     }
+    return payload
+
+def fetch_and_store_youtube_data(channel_url):
+    referer_url = 'https://socialblade.com/youtube/channel/UCq-Fj5jknLsUf-MWSy4_brA'
+
     headers = {
-        'Authorization': 'Basic cHJvbXVsZ2F0ZTpwcm9tdWxnYXRl',
-        'Content-Type': 'application/json'
+        'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7',
+        'Referer': referer_url,
     }
 
-    response = requests.post(url, headers=headers, json=payload)
+    try:
+        request = urllib.request.Request(channel_url, None, headers)
+        uClient = urllib.request.urlopen(request)
+        page_html = uClient.read()
+        uClient.close()
 
-    print(response.text)
+        page_soup = BeautifulSoup(page_html, 'html.parser')
+        channel_id = channel_url.split('/')[-1]
 
-def scrape_channel_data(page_soup):
-    # Existing scrape_channel_data function
-    # Extract data from the web scraping
-    uploads = page_soup.findAll("span", {"id": "youtube-stats-header-uploads"})
-    subs = page_soup.findAll("span", {"id": "youtube-stats-header-subs"})
-    views = page_soup.findAll("span", {"id": "youtube-stats-header-views"})
+        mydb = mysql.connector.connect(
+            host="localhost",
+            database="youtube",
+            user="root",
+            password="Princy@123#"
+        )
 
-    # Add additional data scraping logic for the new columns
-    whatch_time = page_soup.findAll("span", {"id": "youtube-stats-header-whatch_time"})
-    total_likes = page_soup.findAll("span", {"id": "youtube-stats-header-total_likes"})
-    playlists = page_soup.findAll("span", {"id": "youtube-stats-header-playlists"})
-    audience_retention = page_soup.findAll("span", {"id": "youtube-stats-header-audience_retention"})
-    recent_videos = page_soup.findAll("span", {"id": "youtube-stats-header-recent_videos"})
-    audience_by_countries = page_soup.findAll("span", {"id": "youtube-stats-header-audience_by_countries"})
-    audience_by_demographics = page_soup.findAll("span", {"id": "youtube-stats-header-audience_by_demographics"})
-    traffic_source = page_soup.findAll("span", {"id": "youtube-stats-header-traffic_source"})
-    external_source = page_soup.findAll("span", {"id": "youtube-stats-header-external_source"})
-    audience_value = page_soup.findAll("span", {"id": "youtube-stats-header-audience"})
+        mycursor = mydb.cursor()
 
-    # Assuming you want to return the text values of uploads, subscribers, and country
-    return [
-        uploads[0].text if uploads else None,
-        subs[0].text if subs else None,
-        views[0].text if views else None,
-        whatch_time[0].text if whatch_time else None,
-        total_likes[0].text if total_likes else None,
-        playlists[0].text if playlists else None,
-        audience_retention[0].text if audience_retention else None,
-        recent_videos[0].text if recent_videos else None,
-        audience_by_countries[0].text if audience_by_countries else None,
-        audience_by_demographics[0].text if audience_by_demographics else None,
-        traffic_source[0].text if traffic_source else None,
-        external_source[0].text if external_source else None,
-        audience_value[0].text if audience_value else None
-    ]
+        table_name = 'scrapping'
+        scraped_data = scrape_channel_data(page_soup, channel_id)
+        placeholders = ', '.join(['%s'] * len(scraped_data))
+        sql = f"INSERT INTO {table_name} (channelId,Views, Whatch_time,videos_published, Total_likes, Playlists, Audience_retention, Recent_videos, Audience_by_countries, Audience_by_demographics, Traffic_source, External_source, Audience, AddedBy) VALUES ({placeholders})"
+        
+        mycursor.execute(sql, [scraped_data[key] for key in scraped_data])
+        mydb.commit()
+        mycursor.close()
+        mydb.close()
 
-def fetch_and_store_youtube_data(channel_url, channelId):
-    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-    headers = {'User-Agent': user_agent}
+        analytics_response = requests.post(analytics_url, headers=analytics_headers, json=scraped_data)
+        print("YouTube Analytics API Response:", analytics_response.text)
+
+    except urllib.error.HTTPError as e:
+        print(f"HTTP Error {e.code}: {e.reason}")
+        print(f"Request URL: {e.url}")
+        print(f"Request Headers: {e.headers}")
+
+@app.route('/fetch_and_store/<channel_id>', methods=['GET'])
+def fetch_and_store(channel_id):
+    channel_url = f'https://socialblade.com/youtube/channel/{channel_id}'
+    fetch_and_store_youtube_data(channel_url)
+    return jsonify({"message": "Data fetched and stored successfully."})
+if __name__ == '__main__':
+    app.run(debug=True)
 
     try:
         request = urllib.request.Request(channel_url, None, headers)
